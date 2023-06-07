812884e56ddf0f22cf56ec6d00d952306eb3d53a
Nkatha_Glory <kinyua6945@gmail.com>
2023-06-07 22:27:09 +0300

    Added a README.md

--- db/migrate/20230606111509_create_author.rb
+++ db/migrate/20230606111509_create_author.rb
@@ -2,7 +2,7 @@ class CreateAuthor < ActiveRecord::Migration[6.1]
   def change
     create_table :authors do |t|
     t.string :name
-    t.string :email
+    t.integer :contact
     t.references :book, foreign_key: true
     t.references :user, foreign_key: true
     end
